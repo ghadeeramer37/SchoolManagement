94573af288dc3ccabce79e77bb10aae88d0c6e61
MohamudOsman <mohamudotman13@gmail.com>
2021-08-17 07:48:44 +0300

    test3 new1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <<<<<<< HEAD
+<<<<<<< HEAD
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -78,6 +79,10 @@
         </main>
     </div>
 </body>
+=======
+
+
+>>>>>>> parent of a63be0b (test3)
 =======
 
 
